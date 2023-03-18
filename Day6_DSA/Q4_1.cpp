50306d6a3d3f0ae2b2d5de5e9216de78f6713ddc
AMIT KUMAR <76860474+AMITKUMAR24-C@users.noreply.github.com>
2023-03-18 22:18:57 +0530

    Right View

---
+++ Day6_DSA/Q4_1.cpp
@@ -0,0 +1,15 @@
+void righttree(TreeNode* root, vector<int>& ans, int level)
+    {
+        if(root==NULL)
+        return;
+        if(ans.size()==level)
+        ans.push_back(root->val);
+        righttree(root->right,ans,level+1);
+        righttree(root->left,ans,level+1);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        righttree(root,ans,0);
+        return ans;
+
+    }
