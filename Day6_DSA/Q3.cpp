4de4855cad449577184df112e69aab38bbdadaa5
AMIT KUMAR <76860474+AMITKUMAR24-C@users.noreply.github.com>
2023-03-18 19:44:05 +0530

    LOT - Line By Line

--- Day6_DSA/Q3.cpp
+++ Day6_DSA/Q3.cpp
@@ -1,1 +1,35 @@
-
+vector<vector<int>> levelOrderlinebyline(TreeNode* root) {
+        vector<vector<int>> ans;
+        vector<int> treelevel;
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(NULL);
+        if(root==NULL)
+        return ans;
+        while(!q.empty())
+        {
+            TreeNode* temp = q.front();
+            q.pop();
+            if(temp==NULL)
+            {
+                ans.push_back(treelevel);
+                treelevel.clear();
+                if(!q.empty())
+                {
+                    q.push(NULL);
+                }
+            }
+            else
+            {
+                treelevel.push_back(temp->val);
+                if(temp->left)
+                {
+                    q.push(temp->left);
+                }
+                if(temp->right)
+                {
+                    q.push(temp->right);
+                }
+            }
+        }
+        return ans;
